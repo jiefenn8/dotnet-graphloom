f1be987b353e8e8c62bd3f55382ad57de084e18e
jiefenn8 <jiefen8nz@gmail.com>
2019-09-02 14:14:13 +1200

    Fix missing r2rmlmap test

--- dotnet-graphloom.UnitTests/test/RDF/R2RML/TriplesMapTest.cs
+++ dotnet-graphloom.UnitTests/test/RDF/R2RML/TriplesMapTest.cs
@@ -1,9 +1,41 @@
-﻿using NUnit.Framework;
+﻿using GraphLoom.Mappers.Api;
+using GraphLoom.Mappers.Rdf.R2rml;
+using Moq;
+using NUnit.Framework;
+using System.Linq;
+using VDS.RDF;
 
 namespace GraphLoom.UnitTests.Rdf.R2rml
 {
     [TestFixture]
     public class TriplesMapTest
     {
+        private TriplesMap _triplesMap;
+
+        [Test]
+        public void WhenPredicateAndObjectMapGiven_ThenReturnNonEmptyList()
+        {
+            _triplesMap = new TriplesMap(null, null);
+            _triplesMap.AddRelationNodePair(Mock.Of<IRelationMap>(), Mock.Of<INodeMap>());
+            bool result = _triplesMap.ListRelationMaps().Any();
+            Assert.That(result, Is.True);
+        }
+
+        public void WhenPredicateAndObjectMapGiven_ThenReturnTrue()
+        {
+            _triplesMap = new TriplesMap(null, null);
+            _triplesMap.AddRelationNodePair(Mock.Of<PredicateMap>(), Mock.Of<ObjectMap>());
+            bool result = _triplesMap.HasRelationNodeMaps();
+            Assert.That(result, Is.True);
+        }
+
+        public void WhenPredicateMapGiven_ThenReturnNodeMap()
+        {
+            PredicateMap predicateMap = new PredicateMap(Mock.Of<IUriNode>());
+            _triplesMap = new TriplesMap(null, null);
+            _triplesMap.AddRelationNodePair(predicateMap, Mock.Of<INodeMap>());
+            INodeMap result = _triplesMap.GetNodeMapWithRelation(predicateMap);
+            Assert.That(result, Is.Not.Null);
+        }
     }
 }
