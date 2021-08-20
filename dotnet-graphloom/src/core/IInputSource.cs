ebb962d5095b9b3415bf6d4f5726354a42826711
Javen Liu <42923689+jiefenn8@users.noreply.github.com>
2021-08-21 01:11:16 +1200

    Update input source API
    Deprecate old method and add alternative method using the new entity result API too handle source data.

--- dotnet-graphloom/src/core/IInputSource.cs
+++ dotnet-graphloom/src/core/IInputSource.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using GraphLoom.Mapper.Core.InputSource;
+using System;
+using System.Collections.Generic;
 
 namespace GraphLoom.Mapper
 {
     public interface IInputSource
     {
+        [Obsolete("This method is deprecated and will be removed in the future.")]
         List<IDictionary<string, string>> GetEntityRecords(string entity);
+        void ExecuteEntityQuery(IEntityReference entityRef, Action action);
     }
 }
