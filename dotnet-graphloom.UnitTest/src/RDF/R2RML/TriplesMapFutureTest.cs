74e67993bbe2cfb3584e693d3a85afd136298571
Javen Liu <42923689+jiefenn8@users.noreply.github.com>
2022-01-28 02:19:59 +1300

    Add triples map implementation

---
+++ dotnet-graphloom.UnitTest/src/RDF/R2RML/TriplesMapFutureTest.cs
@@ -0,0 +1,141 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TriplesMapFutureTest.cs" company="github.com/jiefenn8">
+//     Copyright (c) 2019 - GraphLoom contributors
+//     (github.com/jiefenn8/dotnet-graphloom)
+//
+//     This software is made available under the terms of Apache License,
+//     Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using GraphLoom.Mapper.Core.ConfigMaps;
+using GraphLoom.Mapper.Core.InputSource;
+using GraphLoom.Mapper.RDF.R2RML;
+using Moq;
+using NUnit.Framework;
+using VDS.RDF;
+
+namespace GraphLoom.UnitTest.RDF.R2RML
+{
+    /// <summary>
+    /// Unit test class for <see cref="TriplesMapFuture"/> and its builder.
+    /// </summary>
+    [TestFixture]
+    public class TriplesMapFutureTest
+    {
+        private INodeFactory factory = new NodeFactory();
+        private TriplesMapFuture triplesMap;
+        private static string IdName = "TEST_ID";
+        private static LogicalTable MockLogicalTable = new LogicalTable.Builder(Mock.Of<IEntityReference>()).Build();
+        private static IPropertyMap MockPropertyMap = Mock.Of<IPropertyMap>();
+
+        [SetUp]
+        public void SetUp()
+        {
+            triplesMap = new TriplesMapFuture.Builder("TEST_ID", MockLogicalTable, MockPropertyMap).Build();
+        }
+
+        [Test]
+        public void Return_expected_source_result()
+        {
+            ISourceResult result = triplesMap.GetSourceResult();
+            Assert.That(result, Is.EqualTo(MockLogicalTable));
+        }
+
+        [Test]
+        public void Return_null_when_entity_value_is_null()
+        {
+            Mock.Get(MockPropertyMap).Setup(f => f.GenerateEntityTerm(It.IsAny<IEntity>())).Returns((IUriNode)null);
+            IUriNode result = triplesMap.GenerateEntityTerm(Mock.Of<IEntity>());
+            Assert.That(result, Is.Null);
+        }
+
+        public static IEnumerable<TestCaseData> PredicateObjectMapArguments()
+        {
+            KeyValuePair<IRelationMap, INodeMap> mockKeyPair = new KeyValuePair<IRelationMap, INodeMap>(Mock.Of<IRelationMap>(), Mock.Of<INodeMap>());
+            yield return new TestCaseData(new TriplesMapFuture.Builder(IdName, MockLogicalTable, MockPropertyMap).AddPredicateObjectMap(mockKeyPair), true);
+            yield return new TestCaseData(new TriplesMapFuture.Builder(IdName, MockLogicalTable, MockPropertyMap), false);
+        }
+
+        [Test, TestCaseSource(nameof(PredicateObjectMapArguments))]
+        public void Return_expected_evaluation_when_there_is_predicate_object_map(TriplesMapFuture.Builder builder, bool expected)
+        {
+            TriplesMapFuture triplesMap = builder.Build();
+            bool result = triplesMap.HasNodePairs();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        public static IEnumerable<TestCaseData> EntityClassArguments()
+        {
+            INodeFactory factory = new NodeFactory();
+            IUriNode firstClass = factory.CreateUriNode(UriFactory.Create("http://example.com/ns#CLASS_1"));
+            IUriNode secondClass = factory.CreateUriNode(UriFactory.Create("http://example.com/ns#CLASS_2"));
+            yield return new TestCaseData(new List<IUriNode>(), 0);
+            yield return new TestCaseData(new List<IUriNode>() { firstClass }, 1);
+            yield return new TestCaseData(new List<IUriNode>() { firstClass, secondClass }, 2);
+        }
+
+        [Test, TestCaseSource(nameof(EntityClassArguments))]
+        public void Return_expected_model_with_class_terms(List<IUriNode> classes, int expectedCount)
+        {
+            Mock.Get(MockPropertyMap).Setup(f => f.ListEntityClasses()).Returns(classes);
+            IUriNode entity = factory.CreateUriNode(UriFactory.Create("http://example.com/data/entity"));
+            INode node = factory.CreateUriNode(UriFactory.Create("rdf:type"));
+            TriplesMapFuture triplesMap = new TriplesMapFuture.Builder(IdName, MockLogicalTable, MockPropertyMap).Build();
+            IGraph result = triplesMap.GenerateClassTerms(entity);
+            Assert.That(result.Triples.Count, Is.EqualTo(expectedCount));
+        }
+
+        public static IEnumerable<TestCaseData> PredicateObjectArguments()
+        {
+            INodeFactory factory = new NodeFactory();
+            Func<TriplesMapFuture.Builder> triplesMapBuilderSupplier = () => new TriplesMapFuture.Builder(string.Empty, MockLogicalTable, MockPropertyMap);
+            IRelationMap mockRelationMap = Mock.Of<IRelationMap>();
+            INodeMap mockNodeMap = Mock.Of<INodeMap>();
+            IUriNode predicate = factory.CreateUriNode(UriFactory.Create("http://example.com/data/PREDICATE"));
+            ILiteralNode literal = factory.CreateLiteralNode("VALUE");
+            Mock.Get(mockNodeMap).Setup(f => f.GenerateNodeTerm(It.IsAny<IEntity>())).Returns(literal);
+            Mock.Get(mockRelationMap).Setup(f => f.GenerateRelationTerm(It.IsAny<IEntity>())).Returns(predicate);
+            yield return new TestCaseData(triplesMapBuilderSupplier.Invoke(), 0);
+            yield return new TestCaseData(triplesMapBuilderSupplier.Invoke().AddPredicateObjectMap(new KeyValuePair<IRelationMap, INodeMap>(mockRelationMap, mockNodeMap)), 1);
+        }
+
+        [Test, TestCaseSource(nameof(PredicateObjectArguments))]
+        public void Return_expected_model_with_predicate_object_terms(TriplesMapFuture.Builder builder, int expectedCount)
+        {
+            IUriNode subject = factory.CreateUriNode(UriFactory.Create("http://example.com/data/SUBJECT"));
+            triplesMap = builder.Build();
+            IGraph result = triplesMap.GenerateNodeTerms(subject, Mock.Of<IEntity>());
+            Assert.That(result.Triples.Count, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void Creating_instance_with_no_unique_id_is_not_possible()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new TriplesMapFuture.Builder(null, MockLogicalTable, MockPropertyMap),
+                "ID name must not be null."
+            );
+        }
+
+        [Test]
+        public void Creating_instance_with_no_logical_table_is_not_possible()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new TriplesMapFuture.Builder("ID_NAME", null, MockPropertyMap),
+                "ID name must not be null."
+            );
+        }
+
+        [Test]
+        public void Creating_instance_with_no_subject_map_is_not_possible()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new TriplesMapFuture.Builder("ID_NAME", MockLogicalTable, null),
+                "Subject map must not be null."
+            );
+        }
+    }
+}
--- dotnet-graphloom/src/RDF/R2RML/TriplesMapFuture.cs
+++ dotnet-graphloom/src/RDF/R2RML/TriplesMapFuture.cs
@@ -9,21 +9,215 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using GraphLoom.Mapper.Core.ConfigMaps;
+using GraphLoom.Mapper.Core.InputSource;
+using NLog;
+using VDS.RDF;
 
 namespace GraphLoom.Mapper.RDF.R2RML
 {
     /// <summary>
-    /// 
+    /// Implementation of R2RML TriplesMap with <see cref="IEntityMap"/> interface. 
+    /// This class is an immutable class and requires the use of its builder
+    /// class to populate and create an instance.
     /// </summary>
-    public class TriplesMapFuture
+    public class TriplesMapFuture : IEntityMap
     {
         /// <summary>
-        /// 
+        /// Id name of this triples map instance.
         /// </summary>
-        /// <returns></returns>
-        public LogicalTable GetSourceMap()
+        private readonly string idName;
+
+        /// <summary>
+        /// Logical table associated with this instance.
+        /// </summary>
+        private readonly LogicalTable logicalTable;
+
+        /// <summary>
+        /// Subject map associated with this instance.
+        /// </summary>
+        private readonly IPropertyMap subjectMap;
+
+        /// <summary>
+        /// Collection of predicate object maps associated with this instance.
+        /// </summary>
+        private readonly IDictionary<IRelationMap, INodeMap> predicateObjectMaps;
+
+        /// <summary>
+        /// Collection of predicate referencing object maps associated with this instance.
+        /// </summary>
+        private readonly IDictionary<IRelationMap, INodeMap> predicateRefObjectMaps;
+
+        /// <summary>
+        /// Constructs a TriplesMap with the specified Builder containing the
+        /// properties to populate and initialise this immutable instance.
+        /// </summary>
+        /// <param name="builder">the triples map builder to build from</param>
+        protected TriplesMapFuture(Builder builder)
         {
-            throw new NotImplementedException();
+            _ = builder ?? throw new ArgumentNullException();
+            idName = builder.IdName;
+            logicalTable = builder.LogicalTable;
+            subjectMap = builder.SubjectMap;
+            predicateObjectMaps = builder.PredicateObjectMaps;
+            predicateRefObjectMaps = builder.PredicateRefObjectMaps;
+        }
+
+        /// <inheritdoc/>
+        public IGraph GenerateClassTerms(IUriNode term)
+        {
+            IGraph model = new Graph();
+            subjectMap.ListEntityClasses().ForEach((c) =>
+            {
+                model.Assert(term, model.CreateUriNode("rdf:type"), c);
+            });
+
+            return model;
+        }
+
+        /// <inheritdoc/>
+        public IUriNode GenerateEntityTerm(IEntity entity)
+        {
+            return subjectMap.GenerateEntityTerm(entity);
+        }
+
+        /// <inheritdoc/>
+        public IGraph GenerateNodeTerms(IUriNode term, IEntity entity)
+        {
+            IGraph model = new Graph();
+            foreach (var item in predicateObjectMaps.Keys)
+            {
+                INodeMap objectMap = predicateObjectMaps[item];
+                INode node = objectMap.GenerateNodeTerm(entity);
+                if (!(node is null))
+                {
+                    model.Assert(term, item.GenerateRelationTerm(entity), objectMap.GenerateNodeTerm(entity));
+                }
+            }
+
+            return model;
+        }
+
+        /// <inheritdoc/>
+        public IGraph GenerateRefNodeTerms(IUriNode term, IInputSource source)
+        {
+            IGraph model = new Graph();
+            foreach (var item in predicateRefObjectMaps.Keys)
+            {
+                INodeMap refObjectMap = predicateRefObjectMaps[item];
+                ISourceResult jointLogicalTable = logicalTable.AsJointLogicalTable((RefObjectMap)refObjectMap);
+                jointLogicalTable.ForEachEntity(source, (e) =>
+                {
+                    INode node = refObjectMap.GenerateNodeTerm(e);
+                    if (!(node is null))
+                    {
+                        model.Assert(term, item.GenerateRelationTerm(e), node);
+                    }
+                });
+            }
+
+            return model;
+        }
+
+        /// <inheritdoc/>
+        public string getIdName()
+        {
+            return idName;
+        }
+
+        /// <inheritdoc/>
+        public ISourceResult GetSourceResult()
+        {
+            return logicalTable;
+        }
+
+        /// <inheritdoc/>
+        public bool HasNodePairs()
+        {
+            return predicateObjectMaps.Any();
+        }
+
+        /// <summary>
+        /// Builder class for <see cref="TriplesMapFuture"/>.
+        /// </summary>
+        public class Builder
+        {
+            /// <summary>
+            /// Logger for this class builder.
+            /// </summary>
+            private static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+
+            /// <summary>
+            /// Gets the id name of this triples map.
+            /// </summary>
+            public string IdName { get; private set; }
+
+            /// <summary>
+            /// Gets the logical table associated with this triples map.
+            /// </summary>
+            public LogicalTable LogicalTable { get; private set; }
+
+            /// <summary>
+            /// Gets the subject map associated with this triples map.
+            /// </summary>
+            public IPropertyMap SubjectMap { get; private set; }
+
+            /// <summary>
+            /// Gets the collection of object maps associated with a relation map.
+            /// </summary>
+            public IDictionary<IRelationMap, INodeMap> PredicateObjectMaps { get; } = new Dictionary<IRelationMap, INodeMap>();
+
+            /// <summary>
+            /// Gets the collection of reference object maps associated with a relation map.
+            /// </summary>
+            public IDictionary<IRelationMap, INodeMap> PredicateRefObjectMaps { get; } = new Dictionary<IRelationMap, INodeMap>();
+
+            /// <summary>
+            /// Constructs a TriplesMap Builder with the specified triples map id,
+            /// logical table and subject map instance.
+            /// </summary>
+            /// <param name="idName">the name of this triples map definition</param>
+            /// <param name="logicalTable">the logical table to set on this triples map</param>
+            /// <param name="subjectMap">the subject map to set on this triples map</param>
+            public Builder(string idName, LogicalTable logicalTable, IPropertyMap subjectMap)
+            {
+                IdName = idName ?? throw new ArgumentNullException("ID name must not be null.");
+                LogicalTable = logicalTable ?? throw new ArgumentNullException("Logical table must not be null.");
+                SubjectMap = subjectMap ?? throw new ArgumentNullException("Subject map must not be null.");
+            }
+
+            /// <summary>
+            /// Adds a predicate map and object map pair to this triples map.
+            /// </summary>
+            /// <param name="pom">the pair to add to triples-map</param>
+            /// <returns>this builder for fluent method chaining</returns>
+            public Builder AddPredicateObjectMap(KeyValuePair<IRelationMap, INodeMap> pom)
+            {
+                INodeMap nodeMap = pom.Value;
+                if (nodeMap is RefObjectMap)
+                {
+                    PredicateRefObjectMaps.Add(pom.Key, nodeMap);
+                    return this;
+                }
+
+                PredicateObjectMaps.Add(pom.Key, nodeMap);
+                return this;
+            }
+
+            /// <summary>
+            /// Returns an immutable instance of triples maps containing the properties
+            /// given to its builder.
+            /// </summary>
+            /// <returns>instance of triples map created with the info in this builder</returns>
+            public TriplesMapFuture Build()
+            {
+                LOGGER.Debug("Building TriplesMap from parameters. ID:{}", IdName);
+                TriplesMapFuture triplesMap = new TriplesMapFuture(this);
+                LOGGER.Debug("{}", triplesMap);
+                return triplesMap;
+            }
         }
     }
 }
