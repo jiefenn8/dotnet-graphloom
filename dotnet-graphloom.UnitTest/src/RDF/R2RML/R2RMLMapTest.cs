bbc9f711b0e218c74d269d17edde6a58d6cd3bc7
Javen Liu <42923689+jiefenn8@users.noreply.github.com>
2022-02-21 23:29:34 +1300

    Add R2RML map implementation

---
+++ dotnet-graphloom.UnitTest/src/RDF/R2RML/R2RMLMapTest.cs
@@ -0,0 +1,78 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="R2RMLMapTest.cs" company="github.com/jiefenn8">
+//     Copyright (c) 2019 - GraphLoom contributors
+//     (github.com/jiefenn8/dotnet-graphloom)
+//
+//     This software is made available under the terms of Apache License,
+//     Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using GraphLoom.Mapper.Core.ConfigMaps;
+using GraphLoom.Mapper.RDF.R2RML;
+using Moq;
+using NUnit.Framework;
+
+namespace GraphLoom.UnitTest.RDF.R2RML
+{
+    /// <summary>
+    /// Unit test class for <see cref="R2RMLMap"/> and its builder.
+    /// </summary>
+    [TestFixture]
+    public class R2RMLMapTest
+    {
+        public R2RMLMap r2rmlMap;
+
+        public static IEnumerable<TestCaseData> NamespaceArguments()
+        {
+            IDictionary<string, string> defaultNamespaceMap = new Dictionary<string, string>();
+            defaultNamespaceMap.Add("rr", "http://www.w3.org/ns/r2rml#");
+            IDictionary<string, string> namespaceMap = new Dictionary<string, string>(defaultNamespaceMap);
+            namespaceMap.Add("ex", "http://example.com/ns/");
+            yield return new TestCaseData(new R2RMLMap.Builder(), defaultNamespaceMap);
+            yield return new TestCaseData(new R2RMLMap.Builder().AddNsPrefix("ex", "http://example.com/ns/"), namespaceMap);
+        }
+
+        [Test, TestCaseSource(nameof(NamespaceArguments))]
+        public void Return_expected_namespace_map(R2RMLMap.Builder builder, IDictionary<string, string> expected) 
+        {
+            r2rmlMap = builder.Build();
+            IDictionary<string, string> result = r2rmlMap.GetNamespaceMap();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        public static IEnumerable<TestCaseData> TriplesMapArguments()
+        {
+            IEntityMap mockTriplesMap = Mock.Of<IEntityMap>();
+            Func<R2RMLMap.Builder> r2rmlMapBuilderSupplier = () => new R2RMLMap.Builder();
+            R2RMLMap.Builder builder = r2rmlMapBuilderSupplier.Invoke();
+            ISet<IEntityMap> expected = new HashSet<IEntityMap>();
+
+            //Add triplesmap to builder starting from none to two for each return.
+            for(int i = 0; i <= 2; i++)
+            {
+                yield return new TestCaseData(builder, expected);
+                IEntityMap mockEntityMap = Mock.Of<IEntityMap>();
+                builder.AddTriplesMap(mockEntityMap);
+                expected.Add(mockEntityMap);
+            }
+        }
+
+        [Test, TestCaseSource (nameof(TriplesMapArguments))]
+        public void Return_expected_triples_map_collection(R2RMLMap.Builder builder, ISet<IEntityMap> expected)
+        {
+            R2RMLMap r2rmlMap = builder.Build();
+            Assert.That(r2rmlMap.GetEntityMaps(), Is.EqualTo(expected));
+        }
+
+        [Test, TestCaseSource(nameof(TriplesMapArguments))]
+        public void Return_expected_enumerator_from_instance(R2RMLMap.Builder builder, ISet<IEntityMap> expected)
+        {
+            R2RMLMap r2rmlMap = builder.Build();
+            IEnumerable<IEntityMap> result = r2rmlMap;
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+    }
+}
--- dotnet-graphloom/src/RDF/R2RML/R2RMLMap.cs
+++ dotnet-graphloom/src/RDF/R2RML/R2RMLMap.cs
@@ -1,31 +1,175 @@
-﻿using System.Collections.Generic;
+﻿//-----------------------------------------------------------------------
+// <copyright file="R2RMLMap.cs" company="github.com/jiefenn8">
+//     Copyright (c) 2019 - GraphLoom contributors
+//     (github.com/jiefenn8/dotnet-graphloom)
+//
+//     This software is made available under the terms of Apache License,
+//     Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
 using GraphLoom.Mapper.Configuration;
+using GraphLoom.Mapper.Core;
+using GraphLoom.Mapper.Core.ConfigMaps;
 
 namespace GraphLoom.Mapper.RDF.R2RML
 {
-    public class R2RMLMap : IMapperConfig
+    /// <summary>
+    /// Implementation of R2RMLMap with <see cref="IMapperConfig"/> and
+    /// <see cref="IConfigMaps"/> omterfaces. 
+    /// This class is an immutable class and requires the use of its builder
+    /// class to populate and create an instance. 
+    /// </summary>
+    public class R2RMLMap : IMapperConfig, IConfigMaps
     {
-        private IDictionary<string, string> _namespaceMap = new Dictionary<string, string>();
-        private List<IStatementsConfig> _statementsConfigs = new List<IStatementsConfig>();
+        /// <summary>
+        /// Collection of prefix namespaces.
+        /// </summary>
+        private readonly IDictionary<string, string> prefixNamespace;
 
+        /// <summary>
+        /// Collection of triples map associated with this R2RML map.
+        /// To be deprecated.
+        /// </summary>
+        private readonly List<IStatementsConfig> _statementsConfigs = new List<IStatementsConfig>();
+
+        /// <summary>
+        /// Collection of triples map associated with this R2RML map.
+        /// </summary>
+        private readonly ISet<IEntityMap> triplesMaps;
+
+        /// <summary>
+        /// Previous default constructor. 
+        /// To be deprecated. 
+        /// </summary>
+        public R2RMLMap() 
+        { 
+            prefixNamespace = new Dictionary<string, string>();
+            triplesMaps = new HashSet<IEntityMap>();   
+        }
+
+        /// <summary>
+        /// Constructs a R2RMLMap with the specified Builder containing the
+        /// properties to populate and initialise this immutable instance. 
+        /// </summary>
+        /// <param name="builder">the r2rml map builder to build from</param>
+        public R2RMLMap(Builder builder)
+        {
+            prefixNamespace = builder.NsPrefixMap;
+            triplesMaps = builder.TriplesMaps;
+        }
+
+        /// <inheritdoc/>
         public IDictionary<string, string> ListNamespaces()
         {
-            return _namespaceMap;
+            return prefixNamespace;
         }
 
+        /// <inheritdoc/>
         public void AddNamespace(string prefix, string ns)
         {
-            _namespaceMap.Add(prefix, ns);
+            prefixNamespace.Add(prefix, ns);
         }
 
+        /// <inheritdoc/>
         public List<IStatementsConfig> ListStatementsConfigs()
         {
             return _statementsConfigs;
         }
 
+        /// <inheritdoc/>
         public void AddStatementsConfig(IStatementsConfig config)
         {
             _statementsConfigs.Add(config);
+        }
+
+        /// <inheritdoc/>
+        public IDictionary<string, string> GetNamespaceMap()
+        {
+            return prefixNamespace;
+        }
+
+        /// <inheritdoc/>
+        public ISet<IEntityMap> GetEntityMaps()
+        {
+            return triplesMaps;
+        }
+
+        /// <inheritdoc/>
+        public IEnumerator<IEntityMap> GetEnumerator()
+        {
+            return triplesMaps.GetEnumerator();
+        }
+
+        /// <inheritdoc/>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Builder class for <see cref="R2RMLMap"/>
+        /// </summary>
+        public class Builder
+        {
+            /// <summary>
+            /// R2RML prefix.
+            /// </summary>
+            private readonly string RrPrefix = "rr";
+
+            /// <summary>
+            /// Gets the namespace prefix map.
+            /// </summary>
+            public IDictionary<string, string> NsPrefixMap { get; private set; } = new Dictionary<string, string>();
+            
+            /// <summary>
+            /// Gets the collection of triples map associated with this R2RML map.
+            /// </summary>
+            public ISet<IEntityMap> TriplesMaps { get; private set; } = new HashSet<IEntityMap>();
+
+            /// <summary>
+            /// Adds a namespace to given prefix to this R2RML map.
+            /// </summary>
+            /// <param name="prefix">the given prefix</param>
+            /// <param name="prefixNamespace">the namespace to associate the prefix with</param>
+            /// <returns>this builder for fluent method chaining</returns>
+            public Builder AddNsPrefix(string prefix, string prefixNamespace)
+            {
+                NsPrefixMap.Add(prefix, prefixNamespace);
+                return this;
+            }
+
+            /// <summary>
+            /// Adds a triples map to this R2RML map.
+            /// </summary>
+            /// <param name="triplesMap">the triples map to add</param>
+            /// <returns>this builder for fluent method chaining</returns>
+            public Builder AddTriplesMap(IEntityMap triplesMap)
+            {
+                TriplesMaps.Add(triplesMap);
+                return this;
+            }
+
+            /// <summary>
+            /// Returns an inmmutable instance of R2RML map containing the properties
+            /// given to its builder.
+            /// </summary>
+            /// <returns>instance of R2RML map created with the info in this builder</returns>
+            public R2RMLMap Build()
+            {
+                string value; 
+                if(!NsPrefixMap.TryGetValue(RrPrefix, out value))
+                {
+                    if(value is null)
+                    {
+                        NsPrefixMap.Add(RrPrefix, "http://www.w3.org/ns/r2rml#");
+                    }
+                }
+
+                return new R2RMLMap(this);
+            }
         }
     }
 }
